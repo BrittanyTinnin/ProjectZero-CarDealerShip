e808cc626a2836ead10ebb0e08f8fb7f1340266a
Brittany Tinnin <tinnin.brittany.n@gmail.com>
2020-11-25 20:24:05 -0700

    added loggers to computer file

--- src/model/layer/Computer.java
+++ src/model/layer/Computer.java
@@ -20,7 +20,7 @@ public class Computer {
 
     private static void car() {
         System.out.println();
-        System.out.println(
+        logger.error(
                 "                              _.-=\"_-         _\n" +
                         "                         _.-=\"   _-          | ||\"\"\"\"\"\"\"---._______     __..\n" +
                         "             ___.===\"\"\"\"-.______-,,,,,,,,,,,,`-''----\" \"\"\"\"\"       \"\"\"\"\"  __'\n" +
@@ -54,13 +54,19 @@ public class Computer {
             customer.getUserData();
             customer.retrieveCarData();
             try {
-                System.out.println("\nChoose an option: ");
-                logger.info("Choose an option: ");
-                System.out.println(
+//                System.out.println("\nChoose an option: ");
+//                logger.info("Choose an option: ");
+//                System.out.println(
+//                        "Are you a customer or employee?\n" +
+//                                "1) Customer\n" +
+//                                "2) Employee\n" +
+//                                "3) Quit"
+//                );
+                logger.info("Choose an option: \n" +
                         "Are you a customer or employee?\n" +
-                                "1) Customer\n" +
-                                "2) Employee\n" +
-                                "3) Quit"
+                        "1) Customer\n" +
+                        "2) Employee\n" +
+                        "3) Quit"
                 );
                 int x = scanner.nextInt();
                 scanner.nextLine();
@@ -76,12 +82,13 @@ public class Computer {
                         quit = true;
                         break;
                     default:
-                        System.out.println("Invalid entry, try again.");
+                        logger.error("Invalid entry, try again.");
                         menu();
                         break;
                 }
             } catch (InputMismatchException e) {
-                System.out.println("Invalid entry, try again.\n");
+//                System.out.println("Invalid entry, try again.\n");
+                logger.error("Invalid entry, try again.");
                 scanner.nextLine();
             }
         }
@@ -98,7 +105,7 @@ public class Computer {
     private static void employeeAccess() {
 
         try {
-            System.out.println("Are you a new employee?\n" +
+            logger.info("Are you a new employee?\n" +
                     "1) Yes\n" +
                     "2) No\n" +
                     "3) Back"
@@ -117,38 +124,47 @@ public class Computer {
                     menu();
                     break;
                 default:
-                    System.out.println("Invalid entry, retry.");
+//                    System.out.println("Invalid entry, retry.");
+                    logger.error("Invalid entry, try again.");
                     customerAccess();
                     break;
             }
         } catch (InputMismatchException e) {
-            System.out.println("Invalid entry, try again.");
+//            System.out.println("Invalid entry, try again.");
+            logger.error("Invalid entry, try again.");
             scanner.nextLine();
         }
     }
 
 
     public static void employeeRegister() {
-        System.out.println("What is your first name?");
+//        System.out.println("What is your first name?");
+        logger.info("What is your first name?");
         String firstName = scanner.next();
-        System.out.println("What is your last name?");
+//        System.out.println("What is your last name?");
+        logger.info("What is your last name?");
         String lastName = scanner.next();
 
         if (employee.register(firstName, lastName)) {
-            System.out.println("Employee successfully register");
+//            System.out.println("Employee successfully register");
+            logger.info("Employee successfully register");
         } else {
-            System.out.println("Employee already exists.");
+//            System.out.println("Employee already exists.");
+            logger.info("Employee already exists.");
         }
     }
 
 
     public static void employeeLogin() {
-        System.out.println("Enter first name:");
+//        System.out.println("Enter first name:");
+        logger.info("Enter first name:");
         String firstName = scanner.next();
-        System.out.println("Enter last name:");
+//        System.out.println("Enter last name:");
+        logger.info("Enter last name:");
         String lastName = scanner.next();
         if (employee.login(firstName, lastName) != null) {
-            System.out.println("Employee logged in.\n");
+//            System.out.println("Employee logged in.\n");
+            logger.info("Employee logged in.");
             employee.employeeMenu();
         }
     }
@@ -179,24 +195,28 @@ public class Computer {
 
             switch (choice) {
                 case 1:
-                    System.out.println("customer register");
+//                    System.out.println("customer register");
+                    logger.info("customer register");
                     customerRegister();
                     break;
                 case 2:
-                    System.out.println("customer Login");
+//                    System.out.println("customer Login");
+                    logger.info("customer login");
                     customerLogin();
                     break;
                 case 3:
                     menu();
                     break;
                 default:
-                    System.out.println("Invalid entry, retry.\n");
+//                    System.out.println("Invalid entry, retry.\n");
+                    logger.error("Invalid entry, retry.");
                     customerAccess();
                     break;
             }
 
         } catch (InputMismatchException e) {
-            System.out.println("Invalid entry, retry.\n");
+//            System.out.println("Invalid entry, retry.\n");
+            logger.error("Invalid entry, retry.");
             scanner.nextLine();
 
         }
@@ -205,28 +225,33 @@ public class Computer {
 
 
     public static void customerRegister() {
-
-        System.out.println("What is your first name?");
+//        System.out.println("What is your first name?");
+        logger.info("What is your first name?");
         String firstName = scanner.next();
-        System.out.println("What is your last name?");
+//        System.out.println("What is your last name?");
+        logger.info("What is your last name?");
         String lastName = scanner.next();
 
         if (customer.register(firstName, lastName)) {
-            System.out.println("Customer successfully register");
+//            System.out.println("Customer successfully register");
+            logger.trace("Customer successfully register");
         } else {
-            System.out.println("Customer already exists.");
+//            System.out.println("Customer already exists.");
+            logger.trace("Customer already exists.");
         }
     }
 
 
     public static void customerLogin() {
-
-        System.out.println("Enter first name:");
+//        System.out.println("Enter first name:");
+        logger.info("Enter first name:");
         String firstName = scanner.next();
-        System.out.println("Enter last name:");
+//        System.out.println("Enter last name:");
+        logger.info("Enter last name:");
         String lastName = scanner.next();
         if (customer.login(firstName, lastName) != null) {
-            System.out.println("Customer logged in.\n");
+//            System.out.println("Customer logged in.\n");
+            logger.trace("Customer logged in.");
             customer.customerMenu(customer.login(firstName, lastName));
         }
 
